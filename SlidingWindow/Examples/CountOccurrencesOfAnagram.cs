e4e34aac34742ac2a504d51c1f6707eefdf36d13
ghodrati <ghodrati@samiansoft.com>
2022-05-20 18:40:53 +0430

    feat: add example Count Occurrences of Anagram

---
+++ SlidingWindow/Examples/CountOccurrencesOfAnagram.cs
@@ -0,0 +1,54 @@
+// Count Occurrences of Anagram
+// Given a word and a text, return the count of occurrences of the anagrams of the word in the text.
+class CountOccurrencesOfAnagram
+{
+    internal static int CountAnagram(string text, string word)
+    {
+        var w = word.Length;
+        var count = 0;
+        var ana = "";
+        var d = new Dictionary<string, int>
+        {
+        };
+        foreach (var i in Enumerable.Range(0, w))
+        {
+            ana += text[i];
+        }
+        if (IsAnagram(ana, word))
+        {
+            count += 1;
+        }
+        foreach (var i in Enumerable.Range(1, text.Length - 1))
+        {
+            ana = ana[1..] + text[i];
+            if (!d.Any(a => a.Key == ana) && IsAnagram(ana, word))
+            {
+                count += 1;
+                d[ana] = 0;
+            }
+            d[ana] = 0;
+        }
+        return count;
+    }
+
+    private static bool IsAnagram(string s, string word)
+    {
+        if (s.Length != word.Length)
+        {
+            return false;
+        }
+        var d = new int[26];
+        foreach (var c in s)
+        {
+            d[(char)c - (char)'a'] = 1;
+        }
+        foreach (var c in word)
+        {
+            if (d[(char)c - (char)'a'] == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- SlidingWindow/Program.cs
+++ SlidingWindow/Program.cs
@@ -2,3 +2,5 @@
 // Explanation: Here the subarray [1, 7] is the sum of the maximum sum.
 Console.WriteLine(MaximumSumSubarrayOfSizek.GetMaxSum(new List<int> { 3, 5, 3, 1, 7 }, 2));
 
+// Explanation: Words — got, otg, ogt are anagrams of got.
+Console.WriteLine(CountOccurrencesOfAnagram.CountAnagram("gotxxotgxdogt","got"));
